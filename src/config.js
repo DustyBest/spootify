273b40b4fe32349fe6915dd4bce102bf6b34f4fb
DustyBest <dustybest@gmail.com>
2021-06-02 01:12:24 -0700

    added error handling

--- src/config.js
+++ src/config.js
@@ -12,54 +12,90 @@ const api = {
 
 // GET TOKEN
 export const getToken = async () => {
-  const newToken = await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded', 
-          'Authorization' : 'Basic ' + btoa(`${api.clientId}:${api.clientSecret}`)
-      },
-      body: 'grant_type=client_credentials'
-  });
-  const data = await newToken.json();
-  const token = data.access_token;
-  return token;
+  try {
+    const newToken = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded', 
+            'Authorization' : 'Basic ' + btoa(`${api.clientId}:${api.clientSecret}`)
+        },
+        body: 'grant_type=client_credentials'
+    });
+    if(!newToken.ok) {
+      throw Error('We had a problem getting a token');
+    }
+    const data = await newToken.json();
+    const token = data.access_token;
+    return token;
+  } 
+  catch(error) {
+    console.log(error.message);
+    return false;
+  }  
 }
 
 // GET NEW RELEASES
 export const getNewReleases = async (token) => {
-  const newReleases = await fetch('https://api.spotify.com/v1/browse/new-releases?country=JP&limit=15', {
-      method: 'GET',
-      headers: {
-          'Authorization' : `Bearer ${token}`
-      },
-  });
-  const data = await newReleases.json();
-  const releases = data.albums.items;
-  return releases;
+  try{
+    const newReleases = await fetch('https://api.spotify.com/v1/browse/new-releases?country=JP&limit=15', {
+        method: 'GET',
+        headers: {
+            'Authorization' : `Bearer ${token}`
+        },
+    });
+    if(!newReleases.ok) {
+      throw Error('We had a problem getting new releases');
+    }
+    const data = await newReleases.json();
+    const releases = data.albums.items;
+    return releases;
+  }
+  catch(error) {
+    console.log(error.message);
+    return [];
+  }
 }
 
 // GET FEATURED PLAYLISTS
 export const getFeaturedPlaylists = async (token) => { 
+  try{
   const featuredPlaylists = await fetch('https://api.spotify.com/v1/browse/featured-playlists?country=JP&limit=15', {
       method: 'GET',
       headers: {
           'Authorization' : `Bearer ${token}`
       },
   });
+  if(!featuredPlaylists.ok) {
+    throw Error('We had a problem getting featured playlists');
+  }
   const data = await featuredPlaylists.json();
   const playlists = data.playlists.items;
   return playlists;
+  }
+  catch(error) {
+    console.log(error.message);
+    return [];
+  }
 }
 
 // GET CATEGORIES TO BROWSE
 export const getCategories = async (token) => {
+  try{
   const browseCategories = await fetch('https://api.spotify.com/v1/browse/categories?country=JP&limit=15', {
       method: 'GET',
       headers: {
           'Authorization' : `Bearer ${token}`
       },
   });
+  if(!browseCategories.ok) {
+    throw Error('We had a problem getting categories to browse');
+  }
   const data = await browseCategories.json();
   const categories = data.categories.items;
   return categories
+  }
+  catch(error){
+    console.log(error.message);
+    return [];
+  }
 }
--- src/routes/Discover/components/Discover.js
+++ src/routes/Discover/components/Discover.js
@@ -11,18 +11,27 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
     };
   }
 
+  
   // When the component mounts, I await my call to get a token.
   // Once I've got it, I use it in my other calls to push data into 
   // my state object.
   async componentDidMount() {
-    const token = await getToken()
-    this.setState({newReleases: await getNewReleases(token)});
-    this.setState({playlists: await getFeaturedPlaylists(token)});
-    this.setState({categories: await getCategories(token)});
+    try{
+      const token = await getToken()
+      if(!token){
+        throw Error ('Bad Token Fetch')
+      }
+      this.setState({newReleases: await getNewReleases(token)});
+      this.setState({playlists: await getFeaturedPlaylists(token)});
+      this.setState({categories: await getCategories(token)});
+    }
+    catch(error){
+      alert(error);
+    }
   }
   
   render() {
